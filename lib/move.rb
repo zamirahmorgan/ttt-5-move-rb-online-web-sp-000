7fdcbaee6dd051723ef5b8cda91af7efdd290362
clean-form-8028 <zamirahmo66@gmail.com>
2019-10-18 21:32:08 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,4 @@
-def display_board(board)
+def display_board(board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -7,3 +7,14 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(user_input)
+  users_input.to_i-1
+end
+
+def move(board, index, player)
+  board[index] = player
+end
+
+display_board(board)
+input_to_index(user_input)
+move(board, 0, "X")
